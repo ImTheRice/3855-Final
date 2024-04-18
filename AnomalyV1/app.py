21f27655cf1e34d0bbe1ea463d46cebcf71e4555
live.comRobeenMaharjan <live.comRobeenMaharjan>
2024-04-18 16:30:43 -0700

    Refactor anomaly detection and logging in app.py, and update port number in app.run()

--- AnomalyV1/app.py
+++ AnomalyV1/app.py
@@ -152,4 +152,4 @@ if __name__ == '__main__':
     t1 = Thread(target=consume_messages)
     t1.setDaemon(True)
     t1.start()
-    app.run(host='0.0.0.0', port=8200)
+    app.run(host='0.0.0.0', port=8180)
